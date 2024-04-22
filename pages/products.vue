5018146511613022227f4edb5a96246bc09da816
Андрій Свирида <andrijsvirida2@gmail.com>
2024-04-22 22:27:23 +0300

    feat: added products data fetching from a Dummy API

--- pages/products.vue
+++ pages/products.vue
@@ -1,139 +1,66 @@
 <script setup lang="ts">
-const columns = [{
-  key: 'id',
-  label: 'ID'
-}, {
-  key: 'name',
-  label: 'Name',
-  sortable: true
-}, {
-  key: 'title',
-  label: 'Title',
-  sortable: true
-}, {
-  key: 'email',
-  label: 'Email',
-  sortable: true,
-  direction: 'desc' as const
-}, {
-  key: 'role',
-  label: 'Role'
-}]
-
-const people = [{
-  id: 1,
-  name: 'Lindsay Walton',
-  title: 'Front-end Developer',
-  email: 'lindsay.walton@example.com',
-  role: 'Member'
-}, {
-  id: 2,
-  name: 'Courtney Henry',
-  title: 'Designer',
-  email: 'courtney.henry@example.com',
-  role: 'Admin'
-}, {
-  id: 3,
-  name: 'Tom Cook',
-  title: 'Director of Product',
-  email: 'tom.cook@example.com',
-  role: 'Member'
-}, {
-  id: 4,
-  name: 'Whitney Francis',
-  title: 'Copywriter',
-  email: 'whitney.francis@example.com',
-  role: 'Admin'
-}, {
-  id: 5,
-  name: 'Leonard Krasner',
-  title: 'Senior Designer',
-  email: 'leonard.krasner@example.com',
-  role: 'Owner'
-}, {
-  id: 6,
-  name: 'Floyd Miles',
-  title: 'Principal Designer',
-  email: 'floyd.miles@example.com',
-  role: 'Member'
-}, {
-  id: 7,
-  name: 'Emily Selman',
-  title: 'VP, User Experience',
-  email: '',
-  role: 'Admin'
-}, {
-  id: 8,
-  name: 'Kristin Watson',
-  title: 'VP, Human Resources',
-  email: '',
-  role: 'Member'
-}, {
-  id: 9,
-  name: 'Emma Watson',
-  title: 'Front-end Developer',
-  email: '',
-  role: 'Member'
-}, {
-  id: 10,
-  name: 'John Doe',
-  title: 'Designer',
-  email: '',
-  role: 'Admin'
-}, {
-  id: 11,
-  name: 'Jane Doe',
-  title: 'Director of Product',
-  email: '',
-  role: 'Member'
-}, {
-  id: 12,
-  name: 'John Smith',
-  title: 'Copywriter',
-  email: '',
-  role: 'Admin'
-}, {
-  id: 13,
-  name: 'Jane Smith',
-  title: 'Senior Designer',
-  email: '',
-  role: 'Owner'
-}]
+const columns = [
+  {
+    key: "id",
+    label: "ID",
+  },
+  {
+    key: "title",
+    label: "Title",
+    sortable: true,
+  },
+  {
+    key: "description",
+    label: "Description",
+    sortable: true,
+  },
+  {
+    key: "price",
+    label: "Price",
+    sortable: true,
+  },
+  {
+    key: "rating",
+    label: "Rating",
+    sortable: true,
+  },
+  {
+    key: "brand",
+    label: "Brand",
+    sortable: true,
+  },
+  {
+    key: "thumbnail",
+    label: "Thumbnail",
+    sortable: true,
+  },
+];
 
 const sort = ref({
-  column: 'name',
-  direction: 'asc'
-})
+  column: "name",
+  direction: "asc",
+});
 
-const page = ref(1)
-const pageCount = 5
+const page = ref(1);
+const pageCount = 5;
 
-const pending = false;
+const q = ref("");
 
-const q = ref('')
-
-const rows = computed(() => {
-  const compare = (a, b) => {
-    const aVal = a[sort.value.column]
-    const bVal = b[sort.value.column]
-
-    if (sort.value.direction === "desc")
-    {
-      return aVal < bVal;
-    }
-
-    return aVal > bVal;
+const { data: productsResponse, pending } = await useAsyncData(
+  "productsResponse",
+  () =>
+    $fetch(`https://dummyjson.com/products/search`, {
+      params: {
+        q: q.value,
+        limit: pageCount,
+        skip: (page.value - 1) * pageCount,
+        sort: `${sort.value.column}:${sort.value.direction}`,
+      },
+    }),
+  {
+    watch: [page, q, sort],
   }
-
-  const lowerSearchText = q.value.toLowerCase()
-
-  const filteredPeople = lowerSearchText.length ? people.filter(p => Object.values(p).some(v => String(v).toLowerCase().includes(lowerSearchText))) : people
-
-  const sortedPeople = filteredPeople.sort((a, b) => compare(a, b) ? 1 : -1)
-
-  return sortedPeople.slice((page.value - 1) * pageCount, (page.value) * pageCount)
-})
-
+);
 </script>
 
 <template>
@@ -142,9 +69,31 @@ const rows = computed(() => {
     <div class="flex px-3 py-3.5 border-b border-gray-200 dark:border-gray-700">
       <UInput v-model="q" placeholder="Filter people..." />
     </div>
-    <UTable v-model:sort="sort" :loading="pending" :columns="columns" :rows="rows" sort-mode="manual" />
-    <div class="flex justify-end px-3 py-3.5 border-t border-gray-200 dark:border-gray-700">
-      <UPagination v-model="page" :page-count="pageCount" :total="people.length" />
+    <UTable
+      v-model:sort="sort"
+      :loading="pending"
+      :columns="columns"
+      :rows="productsResponse.products"
+      sort-mode="manual"
+    >
+      <template #rating-data="{ row }">
+        <span :class="row.rating < 4.5 ? 'text-red-600' : 'text-green-600'">
+          {{ row.rating }}
+        </span>
+      </template>
+      <template #thumbnail-data="{ row }">
+        <img width="100" height="100" :src="row.thumbnail" />
+      </template>
+    </UTable>
+    <div
+      class="flex justify-end px-3 py-3.5 border-t border-gray-200 dark:border-gray-700"
+    >
+      <UPagination
+        v-model="page"
+        :page-count="pageCount"
+        :total="productsResponse.total"
+      />
     </div>
   </div>
-</template>;
+</template>
+;
